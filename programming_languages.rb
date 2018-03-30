46e12f9da75d7accbcf358d84ca05ea5645e282f
eyi1 <ej7729@gmail.com>
2018-03-30 02:24:21 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,7 +9,7 @@ def reformat_languages(languages)
       #{type => interpreted}
       value_hash.each do |value|
         new_hash[type] = {value}
-        
+
   #  :ruby => {}
 
       new_hash[type][:style] = [oo_functional]
