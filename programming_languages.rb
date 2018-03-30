304c503e4f90b2e33b3b2b3d780c8de6e8e87b53
eyi1 <ej7729@gmail.com>
2018-03-30 02:37:30 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,39 +1,29 @@
-# def reformat_languages(languages)
-#   # your code here
-#   new_hash = {}
-#
-#   languages.each do |oo_functional, language|
-#     #oo => ruby
-#     language.each do |type, value_hash|
-#
-#       #{type => interpreted}
-#       value_hash.each do |value|
-#         new_hash[type] = {}
-#
-#   #  :ruby => {key: value, key: value}
-#
-#       new_hash[type][:style] = [oo_functional]
-#         if language == :javascript
-#
-#
-#
-#
-#         end
-#     end
-#   end
-# end
-#   new_hash
-# end
-
-
 def reformat_languages(languages)
-new_hash = {}
-languages.each do |style, language|
-  language.each do |type, attributes|
-  new_hash[type] = attributes
-  new_hash[type][:style]=[]
-  new_hash[type][:style]<<style
+  # your code here
+  new_hash = {}
+
+  languages.each do |oo_functional, language|
+    #oo => ruby
+    language.each do |type, value_hash|
+
+      #{type => interpreted}
+      value_hash.each do |value|
+        new_hash[type] = {}
+
+  #  :ruby => {key: value, key: value}
+
+      # new_hash[type][:style] = [oo_functional]
+      #   if language == :javascript
+      # 
+      # 
+      # 
+      # 
+      #   end
+    end
+  end
 end
+  new_hash
 end
-new_hash
-end
+
+
+
