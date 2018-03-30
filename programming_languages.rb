4434293864cffbe3676168bd166dfef2c84cb788
eyi1 <ej7729@gmail.com>
2018-03-30 01:25:01 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,11 +5,11 @@ def reformat_languages(languages)
   languages.map do |type, language|
     language.map do |key, value|
       new_hash[key] = value
-      #
-      # if languages[language] == "javascript"
-      #   new_hash[key][:style] = [":oo, :functional"]
-      # else
-        new_hash[key][:style] = [type]
+      new_hash[key][:style] = [type]
+        if languages[language] == "javascript"
+            new_hash[key][:style] << languages[:functional]
+            
+      
       # new_hash[key][:style]
     
   end
